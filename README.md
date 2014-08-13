ea770a1d8dffa53d9c37f2f4f5d5b526723b0eab
danieltdp <danieltdp@gmail.com>
2014-08-13 03:23:19 +0000

    First commit. The program uses hachoir as backend It gets the creation date and prints the proper path to be used

--- README.md
+++ README.md
@@ -6,29 +6,30 @@ Small script set to backup photos unidirectionally
 THIS IS FAR FROM READY. I have little time to pursue this project and things here should be broken most of the time
 
 
-# Plan
+Plan
+------
 
-## EXIF and path handling
+### EXIF and path handling
 
 - Make sure I can get from the EXIF the proper info
 - Make sure I can create a usefull path for the backup
 - Treat improper data (bad or missing EXIF and its destination
 - Treat video files
 
-## File comparision
+### File comparision
 
 - Find a way to avoid doing two copies of the same file
 - Register that the photo has been through the backup process
 - Increment this (as when I add glacier to the vanilla HD backup)
 - Compare file binarily
 
-## Log the process
+### Log the process
 
 - Get a sqlite3 database up and running
 - Decide the info that goes there
 - Do it
 
-## UX
+#### UX
 
 - Study UX options (both GUIs multi-platform and terminal-only)
 
---
+++ src/photokeeper.py
@@ -0,0 +1,84 @@
+import datetime
+import sys
+
+# http://hachoir3.readthedocs.org/
+import hachoir_parser
+import hachoir_metadata
+from hachoir_core.error import HachoirError
+from hachoir_core.cmd_line import unicodeFilename
+
+# import exifread
+# def get_exif_date(filename):
+#     f = open(filename,'rb')
+#     keys = exifread.process_file(f,details=False)
+#     f.close()
+#     exif_date = keys['EXIF DateTimeOriginal']
+#     return exif_date.values
+# def get_path(filename):
+#     #exif_date = get_exif_date(filename)
+#     #date_time = datetime.datetime.strptime(exif_date,'%Y:%m:%d %H:%M:%S')
+#     date_time = get_exif_date(filename)
+#     return '%d/%.2d'%(date_time.year, date_time.month)
+
+
+def get_exif_date(filename):
+    filename, realname = unicodeFilename(filename), filename
+    parser = hachoir_parser.createParser(filename, realname)
+    if not parser:
+        print >>stderr, "Unable to parse file"
+        exit(1)
+    try:
+        ## TODO Essa chamada da um warning quando nao ha gps data
+        metadata = hachoir_metadata.extractMetadata(parser)
+    except HachoirError, err:
+        print "Metadata extraction error: %s" % unicode(err)
+        metadata = None
+    if not metadata:
+        print "Unable to extract metadata"
+        exit(1)
+    return metadata.get('creation_date')
+
+def get_path(filename):
+    date_time = get_exif_date(filename)
+    return '%d/%.2d'%(date_time.year, date_time.month)
+
+files = ('../data/video.mp4','../data/DSC_0003.JPG')
+#filename = 'teste.jpg'
+for filename in files:
+    photo_path = get_path(filename)
+    print '%s: %s'%(filename,photo_path)
+
+
+
+
+
+
+# ----------------------
+# testes
+# -----------------------
+# filename, realname = unicodeFilename(filename), filename
+# parser = createParser(filename, realname)
+# if not parser:
+#     print >>stderr, "Unable to parse file"
+#     exit(1)
+# try:
+#     ## TODO Essa chamada da um warning quando nao ha gps data
+#     metadata = hachoir_metadata.extractMetadata(parser)
+# except HachoirError, err:
+#     print "Metadata extraction error: %s" % unicode(err)
+#     metadata = None
+# if not metadata:
+#     print "Unable to extract metadata"
+#     exit(1)
+# for item in metadata:
+#     sys.stdout.write('%s :: '%item.key)
+#     for value in item.values:
+#         sys.stdout.write('%s :: '%value.value)
+#     sys.stdout.write('\n')
+#metadata.get('creation_date')
+
+#print dir(metadata)
+#text = metadata.exportPlaintext()
+#charset = getTerminalCharset()
+#for line in text:
+    #print makePrintable(line, charset)
